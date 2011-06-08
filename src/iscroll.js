a58e92d132c0cc3aef538da8fe5814a9b3efea07
David Alan Hjelle <dahjelle+github.com@thehjellejar.com>
2011-06-08 10:28:07 -0500

    Replaced missing that with this.

--- src/iscroll.js
+++ src/iscroll.js
@@ -873,9 +873,9 @@ iScroll.prototype = {
 		this.enabled = false;
 
 		// If disabled after touchstart we make sure that there are no left over events
-		that._unbind(MOVE_EV);
-		that._unbind(END_EV);
-		that._unbind(CANCEL_EV);
+		this._unbind(MOVE_EV);
+		this._unbind(END_EV);
+		this._unbind(CANCEL_EV);
 	},
 	
 	enable: function () {
