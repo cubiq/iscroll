cubiq/iscroll

commit 43bacc20ad3ff401904331c1cd2dc5550a7991cf

Edited src/iscroll.js via GitHub

src/iscroll.js
@@ -104,7 +104,13 @@ var m = Math,
 			onZoom: null,
 			onZoomEnd: null
 		};
-
+		// Helpers FIX ANDROID BUG!
+		// translate3d and scale doesn't work together! 
+		// Ignoring 3d ONLY WHEN YOU SET that.zoom
+		if ( that.zoom && isAndroid ){
+			trnOpen = 'translate(';
+			trnClose = ')';
+		}
 		// User defined options
 		for (i in options) that.options[i] = options[i];
 		

commit 8102a9c3489c623df17d37e58b61e17ae643cf49

Commit 43bacc20ad3ff401904331c1cd2dc5550a7991cf broke iscroll for some Android devices, check against zoom boolean, not function
 Please enter the commit message for your changes. Lines starting

src/iscroll.js
@@ -107,13 +107,7 @@ var m = Math,
 			onZoom: null,
 			onZoomEnd: null
 		};
-		// Helpers FIX ANDROID BUG!
-		// translate3d and scale doesn't work together! 
-		// Ignoring 3d ONLY WHEN YOU SET that.zoom
-		if ( that.zoom && isAndroid ){
-			trnOpen = 'translate(';
-			trnClose = ')';
-		}
+
 		// User defined options
 		for (i in options) that.options[i] = options[i];
 		
@@ -127,6 +121,14 @@ var m = Math,
 		that.options.vScrollbar = that.options.vScroll && that.options.vScrollbar;
 		that.options.zoom = that.options.useTransform && that.options.zoom;
 		that.options.useTransition = hasTransitionEnd && that.options.useTransition;
+
+		// Helpers FIX ANDROID BUG!
+		// translate3d and scale doesn't work together! 
+		// Ignoring 3d ONLY WHEN YOU SET that.options.zoom
+		if ( that.options.zoom && isAndroid ){
+			trnOpen = 'translate(';
+			trnClose = ')';
+		}
 		
 		// Set some default styles
 		that.scroller.style[vendor + 'TransitionProperty'] = that.options.useTransform ? '-' + vendor.toLowerCase() + '-transform' : 'top left';