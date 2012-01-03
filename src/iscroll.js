4ee2362574e12cbdeae4ab10a4a94acd8d169e62
Ian Gilman <ian@iangilman.com>
2012-01-03 14:53:47 -0800

    Proper vendor prefix for IE

--- src/iscroll.js
+++ src/iscroll.js
@@ -7,6 +7,7 @@ var m = Math,
 	mround = function (r) { return r >> 0; },
 	vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' :
 		(/firefox/i).test(navigator.userAgent) ? 'Moz' :
+		(/trident/i).test(navigator.userAgent) ? 'ms' :
 		'opera' in window ? 'O' : '',
 
     // Browser capabilities
