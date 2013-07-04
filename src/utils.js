b835a654a6c0cbff04556ad25015d60006109faf
Matteo Spinelli <matteo@cubiq.org>
2013-07-04 16:24:47 +0200

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -182,8 +182,8 @@ var utils = (function () {
 		elastic: {
 			style: '',
 			fn: function (k) {
-				var f = 0.22;
-				var e = 0.4;
+				var f = 0.22,
+					e = 0.4;
 
 				if ( k === 0 ) { return 0; }
 				if ( k == 1 ) { return 1; }
