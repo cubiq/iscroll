068b2e2141f00f3cf115a9c395042b3e32eae3e8
Kirill Shirinkin <fodojyko@gmail.com>
2013-11-09 15:36:15 +0100

    [#521] Fix regexp for calculating top position with useTransform:false

--- build/iscroll-probe.js
+++ build/iscroll-probe.js
@@ -877,8 +877,8 @@ IScroll.prototype = {
 			x = +(matrix[12] || matrix[4]);
 			y = +(matrix[13] || matrix[5]);
 		} else {
-			x = +matrix.left.replace(/[^-\d]/g, '');
-			y = +matrix.top.replace(/[^-\d]/g, '');
+			x = +matrix.left.replace(/[^-\d.]/g, '');
+			y = +matrix.top.replace(/[^-\d.]/g, '');
 		}
 
 		return { x: x, y: y };
