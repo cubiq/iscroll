a6bfaddf3f3044d4a70b5ad01ab3080d46594857
Guilherme Souza <guivideojob@gmail.com>
2014-11-24 11:50:40 -0200

    Generating a new build

--- build/iscroll-probe.js
+++ build/iscroll-probe.js
@@ -258,7 +258,7 @@ function IScroll (el, options) {
 
 		snapThreshold: 0.334,
 
-// INSERT POINT: OPTIONS 
+// INSERT POINT: OPTIONS
 
 		startX: 0,
 		startY: 0,
@@ -318,7 +318,7 @@ function IScroll (el, options) {
 
 // INSERT POINT: NORMALIZATION
 
-	// Some defaults	
+	// Some defaults
 	this.x = 0;
 	this.y = 0;
 	this.directionX = 0;
@@ -761,6 +761,8 @@ IScroll.prototype = {
 		easing = easing || utils.ease.circular;
 
 		this.isInTransition = this.options.useTransition && time > 0;
+
+		this._execEvent('beforeTransition');
 
 		if ( !time || (this.options.useTransition && easing.style) ) {
 			this._transitionTimingFunction(easing.style);
