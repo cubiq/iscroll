008e89f2bcb7df81d960cd1d48bb526e7b816e0a
shuizhongyueming <shuizhongyueming@gmail.com>
2015-06-04 22:40:24 +0800

    the IE 6,7,8 is not support with the rgba, so make a compatible

--- src/indicator/indicator.js
+++ src/indicator/indicator.js
@@ -5,7 +5,7 @@ function createDefaultScrollbar (direction, interactive, type) {
 
 	if ( type === true ) {
 		scrollbar.style.cssText = 'position:absolute;z-index:9999';
-		indicator.style.cssText = '-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;position:absolute;background:rgba(0,0,0,0.5);border:1px solid rgba(255,255,255,0.9);border-radius:3px';
+		indicator.style.cssText = '-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;position:absolute;background: #7f7f7f;background:rgba(0,0,0,0.5);border: 1px solid #fafafa;border:1px solid rgba(255,255,255,0.9);border-radius:3px';
 	}
 
 	indicator.className = 'iScrollIndicator';
