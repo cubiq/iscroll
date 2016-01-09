47a91d644650c6e8cfc9aca91e5bb62813fad114
Gleb Mikheev <mikheev@betaagency.ru>
2016-01-09 17:01:03 +0300

    travis: debug

--- test/index.js
+++ test/index.js
@@ -1,8 +1,27 @@
 global.chai = require('chai');
 global.expect = global.chai.expect;
 
+/* RAF immitation */
+global.window = global;
+var rafCallbacks = [];
+global.requestAnimationFrame = function(callback) {
+  return rafCallbacks.push(callback);
+};
+
+global.cancelAnimationFrame = function(index) {
+  rafCallbacks.splice(index - 1);
+};
+global.nextFrame = function() {
+  var callbacks = rafCallbacks;
+  rafCallbacks = [];
+  for (var i in callbacks) {
+    callbacks[i]();
+  }
+};
+/* ///////////////// */
+
 // Load test suites
 require('../src/libs/fps.spec.js');
-require('../src/mixins/EventEmitter.spec.js');
-require('../src/mixins/EventProcessor.spec.js');
-require('../src/components/RenderLayer.spec.js');
+// require('../src/mixins/EventEmitter.spec.js');
+// require('../src/mixins/EventProcessor.spec.js');
+// require('../src/components/RenderLayer.spec.js');
