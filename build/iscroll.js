56133f5ee069dec2009b356521a980c3e12e0532
Julie Wongbandue <julie.wongbandue@deutschinc.com>
2018-10-01 14:32:02 -0400

    included BUTTON for click events

--- build/iscroll.js
+++ build/iscroll.js
@@ -257,7 +257,7 @@ var utils = (function () {
 		var target = e.target,
 			ev;
 
-		if ( !(/(SELECT|INPUT|TEXTAREA)/i).test(target.tagName) ) {
+		if ( !(/(SELECT|INPUT|TEXTAREA|BUTTON)/i).test(target.tagName) ) {
 			// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/initMouseEvent
 			// initMouseEvent is deprecated.
 			ev = document.createEvent(window.MouseEvent ? 'MouseEvents' : 'Event');
@@ -769,7 +769,7 @@ IScroll.prototype = {
 
 		this.hasHorizontalScroll	= this.options.scrollX && this.maxScrollX < 0;
 		this.hasVerticalScroll		= this.options.scrollY && this.maxScrollY < 0;
-		
+
 		if ( !this.hasHorizontalScroll ) {
 			this.maxScrollX = 0;
 			this.scrollerWidth = this.wrapperWidth;
@@ -783,7 +783,7 @@ IScroll.prototype = {
 		this.endTime = 0;
 		this.directionX = 0;
 		this.directionY = 0;
-		
+
 		if(utils.hasPointer && !this.options.disablePointer) {
 			// The wrapper should have `touchAction` property for using pointerEvent.
 			this.wrapper.style[utils.style.touchAction] = utils.getTouchAction(this.options.eventPassthrough, true);
@@ -802,7 +802,7 @@ IScroll.prototype = {
 
 // INSERT POINT: _refresh
 
-	},	
+	},
 
 	on: function (type, fn) {
 		if ( !this._events[type] ) {
